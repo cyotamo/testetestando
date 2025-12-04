29576a80669198ba47b50e27e4e425bbf4201538
cyotamo <cyotamo@yahoo.com.br>
2025-12-04 14:07:39 +0200

    Update form.js

--- form.js
+++ form.js
@@ -3,7 +3,7 @@ document.getElementById("meuFormulario").addEventListener("submit", function(e) 
 
     const dados = new FormData(this);
 
-    fetch("AQUI_URL_DO_SEU_WEBAPP", {
+    fetch("https://script.google.com/macros/s/AKfycbz5pTZ-f1Jn_Nm6lHjauaL0xAi7C_QQG6m1RO8kVjEsH2Vds969xJlWswimc6vmu7Nuaw/exec", {
         method: "POST",
         body: dados
     })
